a5d4c374ff461aa5c83ab1e25cf72f980d8b4650
shawnlawyer <shawnlawyer@gmail.com>
2014-10-30 11:05:13 -0400

    aas

--- includes/cards/tools-bit.php
+++ includes/cards/tools-bit.php
@@ -1,7 +1,9 @@
 <div class="headline">DBF To - Tools</div>
 <br>
 <div class="card">
-    <div class="subline"><a href="https://dbftojson.com" target="_blank" hreflang="en">DBF To JSON at dbftojson.com</a></div>
-    <div class="subline"><a href="http://dbftoxml.com" target="_blank" hreflang="en">DBF To XML at dbftoxml.com</a></div>
-    <div class="subline"><a href="http://dbftocsv.com" target="_blank" hreflang="en">DBF To CSV at dbftocsv.com</a></div>
+    <div class="subline"><a href="https://dbftojson.com" target="_blank" hreflang="en">dBase DBF To JSON, Javascript Object Notation at dbftojson.com</a></div>
+    <div class="subline"><a href="http://dbftoxml.com" target="_blank" hreflang="en">dBase DBF To XML at dbftoxml.com</a></div>
+    <div class="subline"><a href="http://dbftocsv.com" target="_blank" hreflang="en">dBase DBF To CSV at dbftocsv.com</a></div>
+    <div class="subline"><a href="http://dbftocsv.com" target="_blank" hreflang="en">dBase DBF To XSLX at dbftoxlsx.com</a></div>
+    <div class="subline"><a href="http://dbftocsv.com" target="_blank" hreflang="en">dBase DBF To HTML at dbftohtml.com</a></div>
 </div>
