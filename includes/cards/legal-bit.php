81303de0ad54c8b848cc0aee9c0e972f40226962
shawnlawyer <shawnlawyer@gmail.com>
2014-10-30 15:17:47 -0400

    sasa

--- includes/cards/legal-bit.php
+++ includes/cards/legal-bit.php
@@ -1,7 +1,7 @@
 <div class="card auto-grid-cell">
     <div class="card-area alignCenter">
         <div class="alignCenter legal-card"><br>
-            <span class="legalline">DBFto - Software As A Service By Shawn Lawyer</span><br><br>
+            <span class="legalline">DBFto<?=$site::$convert_to?> - Software As A Service By Shawn Lawyer</span><br><br>
             <span class="caption">
             shawnlawyer@gmail.com - +1.718.864.2801 - 331 Wilson Ave Swannanoa NC 28778<br>
             Computer Software Design &amp; Computer Programming Services<br><br>
