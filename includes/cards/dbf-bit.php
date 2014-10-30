148c9171b07c9a33a01ed8eae010159df4a84948
shawnlawyer <shawnlawyer@gmail.com>
2014-10-30 13:12:55 -0400

    sasa

--- includes/cards/dbf-bit.php
+++ includes/cards/dbf-bit.php
@@ -1,12 +1,21 @@
-<div class="headline">A Bit on .DBF dbase Files</div>
+<div class="headline">A Bit on .DBF dBase Database Files</div>
 <br>
 <div class="card">
     <div class="content alignLeft" style="max-width:40em;">
         A .DBF is a dBASE file used by databases. DBF means DataBase File and were originally used in dBase II to dBase Version IV.<br>
         <br>
-       dbase DBF is used by Act!, Clipper, FoxPro, Arago, Wordtech, xBase, and other database products, including Microsoft Excel and Microsoft Access and was developed by Ashton-Tate,<br>
+        dbase DBF was one of the first one of it kind an one of the most successful database file formats to date.<br>
         <br>
-        MIME types:<br>
+        Several companies have produced their own of dBase and dBase programming language. Some xBase products are FoxBASE+, FoxPro, Clipper, Act!, Clipper, FoxPro, Arago, Wordtech, xBase, Excel and Access.<br>
+        <br>
+        Wikipedia <a rel="nofollow" href="http://en.wikipedia.org/wiki/DBase" hreflang="en">DBase</a><br>
+        Programming Paradigm(s)<br>
+        Imperative, Declarative<br>
+        Software Developer : C. Wayne Ratliff<br>
+        Company : Ashton-Tate<br>
+        Website <a rel="nofollow" href="http://www.dbase.com/" hreflang="en">www.dbase.com</a><br>
+        <br>
+        MIME type(s):<br>
         application/dbase,<br>
         application/x-dbase,<br>
         application/dbf,<br>
--- includes/html/head-tags.php
+++ includes/html/head-tags.php
@@ -8,4 +8,4 @@
 <meta name="msvalidate.01" content="<?=$site::$meta_msvalidate?>" />
 <meta name="google-site-verification" content="<?=$site::$meta_google_site_verification?>" />
 <title><?=$site::$site_title?></title>
-<link href="/dbfto.css" rel="stylesheet" type="text/css">
+<link href="/dbfto.css" rel="stylesheet" type="text/css">   
