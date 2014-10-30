52fc3d50d149d6288d48a4dea6ed3d2638276448
shawnlawyer <shawnlawyer@gmail.com>
2014-10-30 11:11:24 -0400

    sasa

--- includes/cards/tools-bit.php
+++ includes/cards/tools-bit.php
@@ -1,9 +1,9 @@
 <div class="headline">DBF To - Tools</div>
 <br>
 <div class="card">
-    <div class="subline"><a href="https://dbftojson.com" target="_blank" hreflang="en">dBase DBF To JSON, Javascript Object Notation at dbftojson.com</a></div>
-    <div class="subline"><a href="http://dbftoxml.com" target="_blank" hreflang="en">dBase DBF To XML at dbftoxml.com</a></div>
-    <div class="subline"><a href="http://dbftocsv.com" target="_blank" hreflang="en">dBase DBF To CSV at dbftocsv.com</a></div>
-    <div class="subline"><a href="http://dbftocsv.com" target="_blank" hreflang="en">dBase DBF To XSLX at dbftoxlsx.com</a></div>
-    <div class="subline"><a href="http://dbftocsv.com" target="_blank" hreflang="en">dBase DBF To HTML at dbftohtml.com</a></div>
+    <div class="subline"><a href="https://dbftojson.com" target="_blank" hreflang="en" alt="DBFtoJSON - Free Online dBase DBF to JSON JavaScript Object Notation Converter">DBFtoJSON - Free Online dBase DBF to JSON JavaScript Object Notation Converter at dbftojson.com</a></div>
+    <div class="subline"><a href="http://dbftoxml.com" target="_blank" hreflang="en" alt="DBFtoXML - Free Online dBase DBF to XML Extensible Markup Language Converter">DBFtoXML - Free Online dBase DBF to XML Extensible Markup Language Converter at dbftoxml.com</a></div>
+    <div class="subline"><a href="http://dbftocsv.com" target="_blank" hreflang="en" alt="DBFtoCSV - Free Online dBase DBF to CSV Comma Separated Values Converter at dbftocsv.com">DBFtoCSV - Free Online dBase DBF to CSV Comma Separated Values Converter at dbftocsv.com</a></div>
+    <div class="subline"><a href="http://dbftoxlsx.com" target="_blank" hreflang="en" alt="DBFtoXLSX - Free Online dBase DBF to XLSX Microsoft Office Excel and OpenOffice at dbftoxlsx.com">DBFtoXLSX - Free Online dBase DBF to XLSX Microsoft Office Excel and OpenOffice at dbftoxlsx.com</a></div>
+    <div class="subline"><a href="http://dbftohtml.com" target="_blank" hreflang="en" alt="DBFtoHTML - Free Online dBase DBF to HTML Hypertext Markup Language at dbftohtml.com">DBFtoHTML - Free Online dBase DBF to HTML Hypertext Markup Language at dbftohtml.com</a></div>
 </div>
