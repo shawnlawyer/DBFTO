b0402c91e689a06d7f73acaf70f97b4fbb8fec73
shawnlawyer <shawnlawyer@gmail.com>
2014-10-31 22:26:38 -0400

    sasa

--- www/upload.php
+++ www/upload.php
@@ -40,7 +40,7 @@ if (isset($_FILES["file"])) {
         }
         ?>
         <span class="btn" onclick="$('#DownloadIFrame').src = 'download_records.php';">Records</span>
-        <iframe id="DownloadIFrame" height="0" width="0" src="" style="visibility:hidden;display:none"></iframe>
+        <iframe id="DownloadIFrame" src=""></iframe>
         <br>
         <br>
         <?php        
